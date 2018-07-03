9c3120adef4246bffa0f2d709062c3f5650a6c61
bbathel12 <bricebathel@hotmail.com>
2018-07-03 15:21:36 -0500

    clickable

--- board.js
+++ board.js
@@ -1,11 +1,35 @@
 var game = function(x,y){
     
+    this.canvas;
     this.board = [];
     this.x = x;
     this.y = y;
+    var offset  = $('#canvas').offset(); 
+    this.xOffset = offset.top;
+    this.yOffset = offset.left;
+
+    this.getIndex = function(position){
+        var x = Math.floor( ( position - this.xOffset ) / 20);
+
+        x = x < 0 ? 0 : x;
+        x = x > this.x ? this.x - 1 : x;
+        return x;
+    }
+ 
+    this.click_function =  function(e){
+        
+        var x = this.getIndex(e.clientX);
+        var y = this.getIndex(e.clientY);
+        console.log('clickety', e.clientX,":",x , e.clientY, ":",y);
+        
+        this.board[x][y].current_state = !this.board[x][y].current_state;
+        this.board[x][y].past_state    = this.board[x][y].current_state;
+
+        this.board[x][y].draw(); 
+    };
     
     this.init = function(){
-        var canvas = document.getElementById('canvas');
+        this.canvas = canvas = document.getElementById('canvas');
         context = canvas.getContext('2d');
         context.fillStyle='grey';
         context.fillRect(0,0,600,600);
@@ -15,6 +39,8 @@ var game = function(x,y){
                 this.board[i][j] = new cell(i,j,context,this);
             }
         }
+
+        this.canvas.addEventListener('click',this.click_function.bind(this));
     }
 
     this.getBoard = function(){
@@ -24,14 +50,14 @@ var game = function(x,y){
     this.run_turn = function(){        
         for(var i = 0; i < cells ; i++){
             for(var j = 0; j < cells; j++){
-                game_board[i][j].decide_state(game);
+                this.board[i][j].decide_state(game);
             }
         }
 
         for(var i = 0; i < cells ; i++){
             for(var j = 0; j < cells; j++){
-                game_board[i][j].before_draw();
-                game_board[i][j].draw();
+                this.board[i][j].before_draw();
+                this.board[i][j].draw();
             }
         }
     }
